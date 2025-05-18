c7e7368fc77be1a6855ca4b05002b10e1f65c5aa
itmeJay <me@itmejason.com>
2025-05-17 21:56:38 -0400

    Update dash_damage_app.py

--- dash_damage_app.py
+++ dash_damage_app.py
@@ -83,4 +83,7 @@ def update_damage(n_clicks, lock, atk, cd, fight_sets, benchmark):
     return atk, cd, result
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
