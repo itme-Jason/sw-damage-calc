86bbe328bcf951a2f4081d49bd99b06de026d8b1
itmeJay <me@itmejason.com>
2025-05-17 20:28:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,32 @@ if lock:
     # Track previous values to detect slider activity
     previous_atk = st.session_state.atk
     previous_cd = st.session_state.cd
+    previous_fight = st.session_state.get("fight_sets", 0)
+
+    atk = st.slider("Total ATK", min_value=2000, max_value=4000, step=10, value=previous_atk, key="atk_slider")
+    cd = st.slider("Crit Damage (%)", min_value=120, max_value=300, step=5, value=previous_cd, key="cd_slider")
+        atk_bonus = 1 + 0.08 * fight_sets
+
+    # Detect which one changed and update session state accordingly
+    if fight_sets != previous_fight:
+        st.session_state.fight_sets = fight_sets
+        atk_bonus = 1 + 0.08 * fight_sets
+        crit_multiplier = 1 + (cd - 100) / 100
+        atk = round(benchmark / (atk_bonus * crit_multiplier))
+        st.session_state.atk = atk
+    elif atk != previous_atk:
+        st.session_state.atk = atk
+        crit_multiplier = benchmark / (st.session_state.atk * atk_bonus)
+        st.session_state.cd = round((crit_multiplier - 1) * 100 + 100)
+    elif cd != previous_cd:
+        st.session_state.cd = cd
+        crit_multiplier = 1 + (st.session_state.cd - 100) / 100
+        st.session_state.atk = round(benchmark / (atk_bonus * crit_multiplier))
+
+    atk = st.session_state.atk
+    cd = st.session_state.cd
+    fight_sets = st.session_state.fight_sets
+    atk_bonus = 1 + 0.08 * fight_sets
 
     atk = st.slider("Total ATK", min_value=2000, max_value=4000, step=10, value=previous_atk, key="atk_slider")
     cd = st.slider("Crit Damage (%)", min_value=120, max_value=300, step=5, value=previous_cd, key="cd_slider")
